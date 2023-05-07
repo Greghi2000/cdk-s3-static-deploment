b7e5dd20034e21e237000d08c51df92468e44bff
Greghi2000 <greghi.fumagalli@gmail.com>
2023-05-07 17:46:29 +0100

    Finished CDK script

--- lib/cdk-s3-static-deployment-stack.ts
+++ lib/cdk-s3-static-deployment-stack.ts
@@ -1,16 +1,54 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
-export class CdkS3StaticDeploymentStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class StaticWebsiteStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Replace these values with your own
+    const domainName = 'example.com';
+    const subdomain = 'www';
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3StaticDeploymentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create S3 bucket for website
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Not recommended for production
+    });
+
+    // Create a certificate
+    const certificate = new acm.DnsValidatedCertificate(this, 'WebsiteCertificate', {
+      domainName: `${subdomain}.${domainName}`,
+      hostedZone: route53.HostedZone.fromLookup(this, 'HostedZone', { domainName }),
+      region: 'us-east-1', // CloudFront requires ACM certificates to be in the us-east-1 region
+    });
+
+    // Create CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'WebsiteDistribution', {
+      defaultBehavior: { origin: new cloudfrontOrigins.S3Origin(websiteBucket) },
+      domainNames: [`${subdomain}.${domainName}`],
+      certificate: certificate,
+    });
+
+    // Create Route53 record
+    new route53.ARecord(this, 'WebsiteAliasRecord', {
+      zone: route53.HostedZone.fromLookup(this, 'HostedZone', { domainName }),
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+      recordName: `${subdomain}.${domainName}`,
+    });
+
+    // Deploy website assets to S3
+    new s3deploy.BucketDeployment(this, 'WebsiteDeployment', {
+      sources: [s3deploy.Source.asset('./website')],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
   }
 }
